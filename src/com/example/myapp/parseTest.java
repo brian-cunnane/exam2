72acbacafc075f73497b12438543dad18156e282
Brian Cunnane <g00174469@gmit.ie>
2015-11-17 17:30:53 +0000

    added missing files

--- src/com/example/myapp/parseTest.java
+++ src/com/example/myapp/parseTest.java
@@ -15,9 +15,6 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 
-/**
- * Created by brian on 15/11/2015.
- */
 public class parseTest extends ListActivity {
     String item;
     @Override
@@ -26,22 +23,29 @@ public class parseTest extends ListActivity {
         setContentView(R.layout.main);
 
         SQLiteDatabase db = null;
-        String TABLE_NAME = "books";
-        String col1 = "author";
-        String col2 = "title";
-        String col3 = "genre";
-        String col4 = "price";
-        String col5 = "publish date";
-        String col6 = "description";
+        String TABLE_NAME = "EMPLOYEE";
+        String col1 = "FNAME";
+        String col2 = "MINIT";
+        String col3 = "LNAME";
+        String col4 = "SSN";
+        String col5 = "BDATE";
+        String col6 = "ADDRESS";
+        String col7 = "SEX";
+        String col8 = "SALARY";
+        String col9 = "SUPERSSN";
+        String col10 = "DNO";
+        String[] cols = {col1, col2, col3, col4 , col5, col6, col7, col8, col9, col10};
 
 
         try {
             String SDPath = "data/data/com.example.myapp";
             String dbPath = SDPath + "/myDB.db";
             db = SQLiteDatabase.openDatabase(dbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
-            db.execSQL("drop table " + TABLE_NAME + ";");
+            //if(db != null)
+                db.execSQL("drop table " + TABLE_NAME + ";");
             db.execSQL("CREATE TABLE "+ TABLE_NAME + "(_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + col1 + " TEXT ," + col2 + " TEXT ," + col3 + " TEXT ," + col4 + " TEXT );");
+                    + col1 + " TEXT ," + col2 + " TEXT ," + col3 + " TEXT ," + col4 + " TEXT ," + col5 + " TEXT ,"
+            +col6+ " TEXT ," + col7 + " TEXT ," + col8 + " TEXT ," + col9 + " TEXT ," + col10 + " TEXT );");
         }catch(SQLiteCantOpenDatabaseException CODE){CODE.printStackTrace();}
         catch(SQLiteException SE){SE.printStackTrace();}
 
@@ -50,13 +54,34 @@ public class parseTest extends ListActivity {
         }catch (XmlPullParserException XPPE){XPPE.printStackTrace();}
         catch(IOException IOE){IOE.printStackTrace();
     }
-        String[] items = item.split("\n" + " ");
+        String[] items = item.split("\n");
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
 
-        ContentValues cv = new ContentValues();
+        ContentValues[] cv = new ContentValues[items.length];
+
+
+        for (int i = 0, j = 0; i < items.length; i++,j ++ ) {
+            if(j%10 == 0) {
+                j = 0;
+            }
+            String temp = items[i];
+            ContentValues c = new ContentValues();
+            //should have new ContentValue for each employee
+            c.put(cols[j], temp);
+            cv[i] = c;
+
+        }
+/*
+        ContentValues c = new ContentValues();
+        for(int i = 0, j = 0; i < items.length; i++, j++){
+            String temp = items[i];
+            c.put(cols[j%5],temp);
+            cv[i] = c;
+        }
+*/
         if(db !=null){
             for (int i = 0; i < items.length; i ++){
-                db.insert(TABLE_NAME, null, cv);
+                    db.insert(TABLE_NAME, null, cv[i]);
             }
         }
 
@@ -66,43 +91,58 @@ public String getItemFromXML(Activity activity)throws XmlPullParserException, IO
     StringBuffer sb = new StringBuffer();
     Resources res = activity.getResources();
 
-    XmlResourceParser xrp = res.getXml(R.xml.books);
+    XmlResourceParser xrp = res.getXml(R.xml.employee);
 
-    String author=null, title=null,genre=null,price=null,publish_date=null;
+    String FNAME=null, MINIT=null, LNAME=null, SSN=null, BDATE=null, ADDRESS = null,
+    SEX = null, SALARY = null, SUPERSSN = null, DNO = null;
 
     xrp.next();
     int eventType = xrp.getEventType();
     while(eventType != xrp.END_DOCUMENT){
         if(eventType == xrp.START_TAG){
-            if(xrp.getName().equals("book")){
-                sb.append("\n");
-                sb.append(xrp.getAttributeValue(null, "id") + "\n");
+           /* if(xrp.getName().equals("book")){
+               // sb.append("\n");
+                //sb.append(xrp.getAttributeValue(null, "id"));
             }
-            else if(xrp.getName().equals("author")){
-                author = xrp.nextText();
-                sb.append("\n author: " +author);
+            else*/ if(xrp.getName().equals("FNAME")){
+                FNAME = xrp.nextText();
+                sb.append(FNAME+"\n");
             }
-            else if(xrp.getName().equals("title")){
-                title = xrp.nextText();
-                sb.append("\n" + title);
+            else if(xrp.getName().equals("MINIT")){
+                MINIT = xrp.nextText();
+                sb.append(MINIT+"\n");
             }
-            else if(xrp.getName().equals("genre")){
-                genre = xrp.nextText();
-                sb.append("\nGenre: " + genre);
+            else if(xrp.getName().equals("LNAME")){
+                LNAME = xrp.nextText();
+                sb.append(LNAME+"\n");
             }
-            else if(xrp.getName().equals("price")){
-                price = xrp.nextText();
-                sb.append("\n" + price);
+            else if(xrp.getName().equals("SSN")){
+                SSN = xrp.nextText();
+                sb.append(SSN+"\n");
             }
-            else if(xrp.getName().equals("publish_date")){
-                publish_date = xrp.nextText();
-                sb.append("\n" + publish_date);
+            else if(xrp.getName().equals("BDATE")){
+                BDATE = xrp.nextText();
+                sb.append(BDATE+"\n");
             }
-            else if(xrp.getName().equals("description")){
-                String description  = xrp.nextText();
-                sb.append("\n" + description);
+            else if(xrp.getName().equals("ADDRESS")){
+                ADDRESS  = xrp.nextText();
+                sb.append(ADDRESS);
+            }else if(xrp.getName().equals("SEX")){
+                SEX  = xrp.nextText();
+                sb.append(SEX);
+            }else if(xrp.getName().equals("SALARY")){
+                SALARY  = xrp.nextText();
+                sb.append(SALARY);
+            }else if(xrp.getName().equals("SUPERSSN")){
+                SUPERSSN  = xrp.nextText();
+                sb.append(SUPERSSN);
+            }else if(xrp.getName().equals("DNO")){
+                DNO  = xrp.nextText();
+                sb.append(DNO);
             }
+
         }
+        //sb.append("\n");
         eventType = xrp.next();
     }
     return sb.toString();
